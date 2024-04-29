3342d852856d70dbf1f3a6009d73a503ac292852
Khalid Omer <khalido@eg.poly.edu>
2024-04-29 02:06:59 -0400

    im cooking

--- app.py
+++ app.py
@@ -20,37 +20,80 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        user_type = request.form.get('user_type')
+        password = request.form.get('password')
+        if not user_type or not password:
+            return jsonify({'success': False, 'message': 'Missing email or password'}), 400
+
+        if user_type == "Staff":
+            username = request.form.get('username')
+             # Query the database to check if the user exists
+            with conn.cursor() as cursor:
+                sql = "SELECT * FROM AirlineStaff WHERE username = %s"
+                cursor.execute(sql, (username,))
+                user = cursor.fetchone()
+            if user and check_password_hash(user['password'], password):
+            # User exists and password matches
+            # Log the user in here if you are using Flask-Login
+                return jsonify({'success': True, 'message': 'Login successful'})
+            else:
+                return jsonify({'success': False, 'message': 'Invalid credentials'}), 401
+        else:
+            email = request.form.get('email')
+            with conn.cursor() as cursor:
+                sql = "SELECT * FROM Customer WHERE email = %s"
+                cursor.execute(sql, (email,))
+                user = cursor.fetchone()
+            if user and check_password_hash(user['password'], password):
+            # User exists and password matches
+            # Log the user in here if you are using Flask-Login
+                return jsonify({'success': True, 'message': 'Login successful'})
+            else:
+                return jsonify({'success': False, 'message': 'Invalid credentials'}), 401
+
     return render_template("login.html")
-
-@app.route('/alogin', methods=['POST'])
-def process_login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if not email or not password:
-        return jsonify({'success': False, 'message': 'Missing email or password'}), 400
-
-    if email == "test@example.com" and password == "securepassword":
-        return jsonify({'success': True, 'message': 'Login successful'})
-    else:
-        return jsonify({'success': False, 'message': 'Invalid credentials'}), 401
-
-
-
-#Configure MySQL
-# For MAMP on Mac, add the port or unix_socket AND pwd = "root"
 
 @app.route('/signup', methods=['POST'])
 def process_signup():
     email = request.form.get('email')
     password = request.form.get('password')
-    confirm_password = request.form.get('confirm_password')
-    if not email or not password or not confirm_password:
+    user_type = request.form.get('user_type')
+    if not email or not password or not user_type:
         return jsonify({'success': False, 'message': 'Missing information'}), 400
+ 
+    if user_type == "Staff":
+        username = request.form.get('username')
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM Staff WHERE username = %s"
+            cursor.execute(sql, (username,))
+            existing_user = cursor.fetchone()
 
-    if password != confirm_password:
-        return jsonify({'success': False, 'message': 'Passwords do not match'}), 400
-    
-    # Proceed to create the user account...
+        if existing_user:
+            return jsonify({'success': False, 'message': 'Username already exists'}), 409
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM Staff WHERE email = %s"
+            cursor.execute(sql, (email,))
+            existing_user = cursor.fetchone()
+
+        if existing_user:
+            return jsonify({'success': False, 'message': 'Email already exists'}), 409
+    else:
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM Customer WHERE email = %s"
+            cursor.execute(sql, (username,))
+            existing_user = cursor.fetchone()
+
+        if existing_user:
+            return jsonify({'success': False, 'message': 'Username already exists'}), 409
+ 
+    # If the user does not exist, insert the new user into the database
+    hashed_password = generate_password_hash(password)
+    with conn.cursor() as cursor:
+        sql = "INSERT INTO Customer (email, password) VALUES (%s, %s)"
+        cursor.execute(sql, (email, hashed_password))
+        conn.commit()
+
     return jsonify({'success': True, 'message': 'Signup successful'})
 
 if __name__ == "__main__":
--- static/js/login.js
+++ static/js/login.js
@@ -3,6 +3,7 @@ const loginForm = document.querySelector("form.login");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
+
 signupBtn.onclick = () => {
   loginForm.style.marginLeft = "-50%";
   loginText.style.marginLeft = "-50%";
@@ -11,47 +12,71 @@ loginBtn.onclick = () => {
   loginForm.style.marginLeft = "0%";
   loginText.style.marginLeft = "0%";
 };
-signupLink.onclick = () => {
-  signupBtn.click();
-  return false;
-};
+// signupLink.onclick = () => {
+//   signupBtn.click();
+//   return false;
+// };
 
-$(document).ready(function () {
-  $("form.login").on("submit", function (event) {
-    event.preventDefault(); // Prevent default form submission
-    var form = $(this);
-    $.ajax({
-      url: "/alogin",
-      type: "POST",
-      data: form.serialize(),
-      success: function (response) {
-        alert(response.message);
-        if (response.success) {
-          window.location.href = "/"; // Redirect to login after signup
-        }
-      },
-      error: function (response) {
-        alert(response.responseJSON.message);
-      },
-    });
+document
+  .getElementById("user-type-signup")
+  .addEventListener("change", function () {
+    var userType = this.value;
+    if (userType === "staff") {
+      document.getElementById("username-field-signup").style.display = "block";
+    } else {
+      document.getElementById("username-field-signup").style.display = "none";
+    }
   });
 
-  $("form.signup").on("submit", function (event) {
-    event.preventDefault();
-    var form = $(this);
-    $.ajax({
-      url: "/signup",
-      method: "POST",
-      data: form.serialize(),
-      success: function (response) {
-        alert(response.message);
-        if (response.success) {
-          window.location.href = "/"; // Redirect to login after signup
-        }
-      },
-      error: function (response) {
-        alert(response.responseJSON.message);
-      },
-    });
+document
+  .getElementById("user-type-login")
+  .addEventListener("change", function () {
+    var userType = this.value;
+    if (userType === "staff") {
+      document.getElementById("email-field").style.display = "none";
+      document.getElementById("username-field").style.display = "block";
+    } else {
+      document.getElementById("email-field").style.display = "block";
+      document.getElementById("username-field").style.display = "none";
+    }
   });
-});
+
+// $(document).ready(function () {
+//   $("form.login").on("submit", function (event) {
+//     event.preventDefault(); // Prevent default form submission
+//     var form = $(this);
+//     $.ajax({
+//       url: "/login",
+//       type: "POST",
+//       data: form.serialize(),
+//       success: function (response) {
+//         alert(response.message);
+//         if (response.success) {
+//           window.location.href = "/"; // Redirect to login after signup
+//         }
+//       },
+//       error: function (response) {
+//         alert(response.responseJSON.message);
+//       },
+//     });
+//   });
+
+//   $("form.signup").on("submit", function (event) {
+//     event.preventDefault();
+//     var form = $(this);
+//     $.ajax({
+//       url: "/signup",
+//       method: "POST",
+//       data: form.serialize(),
+//       success: function (response) {
+//         alert(response.message);
+//         if (response.success) {
+//           window.location.href = "/"; // Redirect to login after signup
+//         }
+//       },
+//       error: function (response) {
+//         alert(response.responseJSON.message);
+//       },
+//     });
+//   });
+// });
