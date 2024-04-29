4c39099c9740589f2769ef1241f215aa3eedeb8f
Khalid Omer <khalido@eg.poly.edu>
2024-04-28 23:53:05 -0400

    compeleted landing page, working on user auth

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET','POST0'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(f"Email: {email}, User: {user}")
+
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.movies'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html")
+
+if __name__ == "__main__":
+	app.run(debug = True)
---
+++ static/js/login.js
@@ -0,0 +1,12 @@
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
